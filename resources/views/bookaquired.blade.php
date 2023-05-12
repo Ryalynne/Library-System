acbbef7279d286368895ae55bcce4b035a8f7d84
Ryalynne <santiago_ryan78@yahoo.com>
2023-05-12 10:45:08 +0800

    update

--- resources/views/bookaquired.blade.php
+++ resources/views/bookaquired.blade.php
@@ -106,7 +106,7 @@
                                 <div class="modal-dialog modal-dialog-centered">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="staticBackdropLabel">AJUST COPIES</h1>
+                                            <h1 class="modal-title fs-5" id="staticBackdropLabel">ADJUST COPIES</h1>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                 aria-label="Close"></button>
                                         </div>
--- resources/views/borrowpage.blade.php
+++ resources/views/borrowpage.blade.php
@@ -199,7 +199,7 @@
         <div class="modal-dialog modal-dialog-centered modal-fullscreen">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5">PRINT BORRROW</h1>
+                    <h1 class="modal-title fs-5">PRINT BORROW</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
