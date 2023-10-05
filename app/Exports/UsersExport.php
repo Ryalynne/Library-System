65dfdd4646572c18c1c680cc542b66b517b09f02
Ryalynne <santiagoryan788@gmail.com>
2023-10-05 14:16:18 +0800

    new version 10/5/2023

---
+++ app/Exports/UsersExport.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\booklist;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class UsersExport implements FromCollection, WithHeadings
+{
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function collection()
+    {
+        return booklist::select("title", "author", 'department', 'copyright', 'accession', 'callnumber', 'subject')->where('ishide', 0)->get();
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function headings(): array
+    {
+        return ["TITLE", 'AUTHOR', 'DEPARTMENT', 'COPYRIGHT', 'ACCESSION NO.', 'CALL NO.', 'SUBJECT'];
+    }
+}
